523b6e9de41a27b23712f4943457e61c11e190ad
LuiguiSD48 <125932428+LuiguiSD48@users.noreply.github.com>
2023-02-20 18:45:01 -0600

    definimos metodo de personaje

--- Personaje.py
+++ Personaje.py
@@ -7,4 +7,16 @@ class Personaje:
     altura=1.90
     
     #Metodos
-    
+    def correr(self,status):
+        if(status):
+            print("El personaje "+self.nombre+" esta corriendo")
+        else:
+            print("El personaje "+self.nombre+" se detuvo")
+            
+    def lanzarGranada(self):
+        print("Se lanzo granada")
+        
+    def recargarArma(self,municiones):
+        cargador=5
+        cargador=cargador+municiones
+        print("El arma tiene ahora "+ cargador+" balas")
