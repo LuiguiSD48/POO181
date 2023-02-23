bd901c1dead9859ccdda662e166e067f718b21cc
LuiguiSD48 <125932428+LuiguiSD48@users.noreply.github.com>
2023-02-22 18:38:58 -0600

    Agregamos constructores a los pj

--- objetos.py
+++ objetos.py
@@ -1,11 +1,29 @@
 #Importar pj
 from Personaje import *
 
-#Instanciar un objeto
-Heroe= Personaje()
+#ISolicitar atributos para el obj
+print("")
+print("###Solicitud de datpos de heroe###")
+espH=input("Escribe la especie del heroe: ")
+nomH=input("Escribe el nombre del heroe: ")
+altH=float(input("Escribe la altura del heroe: "))
+cargaH=int(input("Escribe la carga del heroe: "))
+
+print("")
+print("###Solicitud de datos de villano###")
+espV=input("Escribe la especie del villano: ")
+nomV=input("Escribe el nombre del villanio: ")
+altV=float(input("Escribe la altura del villano: "))
+cargaV=int(input("Escribe la carga del villano: "))
+
+#3
+Heroe= Personaje(espH,nomH,altH)
+Villano= Personaje(espV,nomV,altV)
+
+
 
 #Acceder a sus atributos
-print("ATRIBUTOS DE PJ")
+print("ATRIBUTOS DE Heroe")
 print("El personaje pertenece a la raza: "+Heroe.especie)
 print("Se llama: "+Heroe.nombre)
 print("Mide: "+str(Heroe.altura)+" mts")
@@ -14,4 +32,15 @@ print("Mide: "+str(Heroe.altura)+" mts")
 print("METODOS DE PJ")
 Heroe.lanzarGranada()
 Heroe.correr(True)
-Heroe.recargarArma(16)
+Heroe.recargarArma(cargaH)
+
+print("ATRIBUTOS DE Villano")
+print("El personaje pertenece a la raza: "+Villano.especie)
+print("Se llama: "+Villano.nombre)
+print("Mide: "+str(Villano.altura)+" mts")
+
+
+print("METODOS DE PJ")
+Villano.lanzarGranada()
+Villano.correr(False)
+Villano.recargarArma(cargaV)
